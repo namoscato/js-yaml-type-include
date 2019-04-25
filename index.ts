a334faf88d2095f603148235e4ed20c6dd72515d
Julian Hundeloh <julian@hundeloh-consulting.ch>
2019-04-25 15:21:03 +0200

    fix(index.ts): add option to change name

--- index.ts
+++ index.ts
@@ -3,13 +3,15 @@ import path from 'path'
 import yaml from 'js-yaml'
 
 export interface Options {
+  name?: string
   relativeTo?: string
 }
 
 export default function createType({
+  name = 'tag:yaml.org,2002:include',
   relativeTo = process.cwd(),
 }: Options = {}) {
-  return new yaml.Type('tag:yaml.org,2002:include', {
+  return new yaml.Type(name, {
     kind: 'scalar',
     resolve: (includePath: string) => {
       return fs.existsSync(path.resolve(relativeTo, includePath))
